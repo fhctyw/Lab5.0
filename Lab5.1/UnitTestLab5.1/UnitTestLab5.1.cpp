723eee6bd36a3de2c67430cdf8e70960a607119d
fhctyw <lisnychukarsenw@gmail.com>
2021-10-13 15:33:37 +0300

    create unit test

---
+++ Lab5.1/UnitTestLab5.1/UnitTestLab5.1.cpp
@@ -0,0 +1,18 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab5.1/Main.cpp"
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTestLab51
+{
+	TEST_CLASS(UnitTestLab51)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			auto tmp = h(1, 0);  // a = 1, b = 0
+			Assert::AreEqual(1., tmp);   //  expected = 1
+		}
+	};
+}
